592ac61fe7b2d388bdd08a4cfc18d7021874d60f
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-09-12 19:47:05 +0200

    intro

--- sport-app/src/App.js
+++ sport-app/src/App.js
@@ -3,20 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <header>
+        <h1>
+          OLD SPORT
+        </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Opzoek naar oefeningen waarmee jij vandaag het beste uit jezelf haalt? Beantwoord drie korte vragen en ga aan de slag!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>
+          start
+        </button>
       </header>
     </div>
   );
