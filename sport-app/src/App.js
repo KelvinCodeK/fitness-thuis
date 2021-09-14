dc3b0e43f01fb5c75db52acd1050884f363bd011
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-09-14 22:55:36 +0200

    intro

--- sport-app/src/App.js
+++ sport-app/src/App.js
@@ -1,20 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+// voor yt vids gebruik dit kanaal > https://www.youtube.com/watch?v=6ZJbSZNe370&ab_channel=Sportzorg
 
 function App() {
   return (
     <div>
       <header>
         <h1>
-          OLD SPORT
+          Fitnessoefeningen voor thuis
         </h1>
         <p>
-          Opzoek naar oefeningen waarmee jij vandaag het beste uit jezelf haalt? Beantwoord drie korte vragen en ga aan de slag!
+          Opzoek naar oefeningen waarmee jij vandaag het beste uit jezelf haalt? Beantwoord twee korte vragen en ga aan de slag!
         </p>
-        <button>
-          start
-        </button>
       </header>
+      <button>
+        start
+      </button>
     </div>
   );
 }
